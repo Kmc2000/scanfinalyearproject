1dd435d1c881f05de459552a28bbf0d1be2818f3
Coaran Greavison <cg494@kent.ac.uk>
2022-10-24 18:21:40 +0100

    Resolve SP-21 -Implements Screen 2 from the wireframe.

---
+++ ProjectScan/ScanningInProgress.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectScan
+{
+    /// <summary>
+    /// Interaction logic for ScanningInProgress.xaml
+    /// </summary>
+    public partial class ScanningInProgress : Window
+    {
+        public ScanningInProgress()
+        {
+            InitializeComponent();
+            ScanningText.Text = "FILENAME is being scanned..."; // TODO: get filename from MainWindow.
+        }
+    }
+}
