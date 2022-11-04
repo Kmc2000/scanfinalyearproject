c9c79773430de29e8216bffb1f20c308ebcae77d
99Reef <115996268+99Reef@users.noreply.github.com>
2022-11-04 12:14:59 +0000

    Add files via upload
    A small test program for Project scan that prints "Hello, World!"
    i have attached the SHA 256 hash in the comments for later testing.

---
+++ Test Program.cs
@@ -0,0 +1,10 @@
+﻿// A small test program for Project scan that prints "Hello, World!"
+
+// Sha 256 hash - C1E419C30444AAC9253CA73AB7C2DE0E640BF653092982E647AEBE752C831814
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
