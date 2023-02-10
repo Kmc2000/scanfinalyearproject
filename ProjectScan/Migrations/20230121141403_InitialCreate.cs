1ea68a55e85ccfa6afc15121bfb26383513fc5f5
Tochi <Tochitos2@gmail.com>
2023-02-10 14:05:51 +0000

    Initial Implementation of insertion of YARA rules from file directory into database.

--- ProjectScan/Migrations/20230121141403_InitialCreate.cs
+++ ProjectScan/Migrations/20230121141403_InitialCreate.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 #nullable disable
 
@@ -10,6 +11,7 @@ namespace ProjectScan.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Create hash table
             migrationBuilder.CreateTable(
                 name: "KnownBadHashes",
                 columns: table => new
@@ -23,6 +25,22 @@ namespace ProjectScan.Migrations
                 {
                     table.PrimaryKey("PK_KnownBadHashes", x => x.Id);
                 });
+
+            // Create YARA rule table
+            migrationBuilder.CreateTable(
+                name: "KnownBadYaraRules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    YaraRule = table.Column<string>(type: "TEXT", nullable: true),
+                    Categorisation = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KnownBadYaraRules", x => x.Id);
+                });
+
         }
 
         /// <inheritdoc />
@@ -30,6 +48,9 @@ namespace ProjectScan.Migrations
         {
             migrationBuilder.DropTable(
                 name: "KnownBadHashes");
+
+            migrationBuilder.DropTable(
+                name: "KnownBadYaraRules");
         }
     }
 }
